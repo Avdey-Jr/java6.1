387240bdb91b48a79011e7d4df32f23682732f27
Avdey-Jr <92048424+Avdey-Jr@users.noreply.github.com>
2022-10-11 00:39:01 +0300

    Update StatictiksServiceTest.java

--- src/test/java/ru/netology/statictiks/StatictiksServiceTest.java
+++ src/test/java/ru/netology/statictiks/StatictiksServiceTest.java
@@ -18,3 +18,30 @@ import static org.junit.jupiter.api.Assertions.*;
          assertEquals(expected, actual);
      }
  }
+class StatictiksServiceTest{
+    @Test
+    void findMax() {
+        StatictiksService service = new StatictiksService();
+     
+        long[] incomesInBillions = {-5, -12, -8, -4, -5, -3, -8, -6, -11, -11, -12};
+        long expected = -12;
+     
+        long actual = service.findMax(incomeInBillions);
+     
+        assertEquals(expected, actual);
+    }
+}
+
+class StatictiksServiceTest{
+     @Test
+     void findMax() {
+      StaticticksService service = new StatictiksService();
+      
+      long[] incomesInBillions = {0}
+      long expected = 0;
+      
+      long actual = service.findMax(incomeInBillions);
+      
+      assertEquals(expected, actual);
+     }
+}
