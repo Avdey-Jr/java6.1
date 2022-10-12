9e0cf65bf9c2a084c3b509b3bebb3a66e4981eab
Mikhail Avdeev <easymikey@MacBook-Pro-Mikhail.local>
2022-10-12 18:47:41 +0300

    обновил тесты

--- src/test/java/ru/netology/statictiks/StatictiksServiceTest.java
+++ src/test/java/ru/netology/statictiks/StatictiksServiceTest.java
@@ -2,46 +2,40 @@ package ru.netology.statictiks;
 
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
- class StatictiksServiceTest {
-     @Test
-     void findMax() {
-         StatictiksService service = new StatictiksService();
-
-         long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-         long expected = 12;
-
-         long actual = service.findMax(incomesInBillions);
-
-         assertEquals(expected, actual);
-     }
- }
-class StatictiksServiceTest{
+class StatictiksServiceTest {
     @Test
     void findMax() {
         StatictiksService service = new StatictiksService();
-     
-        long[] incomesInBillions = {-5, -12, -8, -4, -5, -3, -8, -6, -11, -11, -12};
-        long expected = -12;
-     
-        long actual = service.findMax(incomeInBillions);
-     
+
+        long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 1};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
         assertEquals(expected, actual);
     }
-}
 
-class StatictiksServiceTest{
-     @Test
-     void findMax() {
-      StaticticksService service = new StatictiksService();
-      
-      long[] incomesInBillions = {0}
-      long expected = 0;
-      
-      long actual = service.findMax(incomeInBillions);
-      
-      assertEquals(expected, actual);
-     }
+    @Test
+    void findMin() {
+        StatictiksService service = new StatictiksService();
+
+        long[] incomesInBillions = {-1, -2, -3, -7, -5, -12, -9, -11, -11, -13};
+        long expected = -1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findZero(){
+        StatictiksService service = new StatictiksService();
+        long[] incomesInBillion = {-1, 0, -2, -5, -7, 0, -12, -11, -3};
+        long expected = 0;
+        long actual = service.findMax(incomesInBillion);
+
+        assertEquals(expected, actual);
+    }
 }
